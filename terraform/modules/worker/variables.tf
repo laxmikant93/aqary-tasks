767a8fa10d1161f5e68cd5ec7bb7d0c3ef3ee98c
Trainee <trainee@finehomeint.com>
2026-05-05 17:41:03 +0400

    add all

--- terraform/modules/worker/variables.tf
+++
@@ -1,5 +0,0 @@
-# variable "ami_id" {}
-# variable "instance_type" { default = "t3.micro" }
-# variable "subnet_id" {}
-# variable "security_group_ids" { type = list(string) }
-# variable "instance_profile" {}
